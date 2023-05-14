bf3b5ff45e5aa4905babc4b25e8885f4728ac17f
Yan Gabriel <yangabrielogy@gmail.com>
2023-05-14 00:40:04 -0300

    fix: código mais limpo e conciso
    gamePhaseHandler mais inteligente
    desempacotamento melhor
    uso de `let` sem motivo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import utils from "../_utils"
 import config from "./config.json"
-import * as requests from "./requests.js"
+import * as requests from "./requests"
 
 /**
  * @author Yan Gabriel <Balaclava#1912>
@@ -14,35 +14,30 @@ const gamePhaseHandler = async message => {
   const messageData = jsonObject[2]["data"]
   if (messageData !== "ChampSelect") { return }
 
-  const intervalId = setInterval(async () => {
-    const gamePhase = await requests.getGamePhase()
-    if (gamePhase !== "ChampSelect") { clearInterval(intervalId) }
-    else {
+  while (await requests.getGamePhase() === "ChampSelect") {
       const championSelectData = await requests.getChampionSelectData()
-      await actionHandler(championSelectData)
+    await onChampionSelect(championSelectData)
+
+    // sair do while caso `championSelectData.timer.phase` for finalization
+    if (championSelectData.timer.phase === "FINALIZATION") { console.log("sucesso!"); return }
+
+    // delay de 1 segundo pra não sobrecarregar o lcu
+    await new Promise(resolve => setTimeout(resolve, 1000))
     }
-  }, 250)
 }
 
-const actionHandler = async championSelectData => {
-  const localPlayerCellId = championSelectData.localPlayerCellId // id do jogador local
-  const actions = championSelectData.actions // ações da seleção de campeão
+const onChampionSelect = async championSelectData => {
+  const { localPlayerCellId, actions, bans, myTeam, theirTeam } = championSelectData
+  const allBans = [...bans.myTeamBans, ...bans.theirTeamBans]
+  const allPicks = [...myTeam, ...theirTeam]
 
-  const teamBans = championSelectData.bans.myTeamBans // banimento dos aliados
-  const enemyBans = championSelectData.bans.theirTeamBans // banimento dos inimigos
-  const allBans = [...teamBans, ...enemyBans] // todos os banimentos
-
-  const teamPicks = championSelectData.myTeam // escolha dos aliados
-  const enemyPicks = championSelectData.theirTeam // escolha dos inimigos
-  const allPicks = [...teamPicks, ...enemyPicks] // todas as escolhas
-
-  for (let subAction of actions) {
-    for (let action of subAction) {
+  for (const subAction of actions) {
+    for (const action of subAction) {
       // buscando apenas por ações do usuário local que não foram completadas
       if (action.completed || action.actorCellId != localPlayerCellId) { continue }
 
       if (action.type === "pick" && pickChampion.enabled) { // se é a vez de escolher um campeão
-        for (let championId of pickChampion.ids) {
+        for (const championId of pickChampion.ids) {
           if (allBans.some(bannedChampionId => bannedChampionId == championId)) { continue } // se alguém já baniu o campeão
           if (allPicks.some(unit => unit.championId == championId)) { continue } // se alguém já pegou o campeão
           if (await requests.selectChampion(action.id, championId)) { return }
@@ -50,9 +45,9 @@ const actionHandler = async championSelectData => {
       }
 
       if (action.type === "ban" && banChampion.enabled) { // se é a vez de banir um campeão
-        for (let championId of banChampion.ids) {
+        for (const championId of banChampion.ids) {
           if (allBans.some(bannedChampionId => bannedChampionId == championId)) { continue } // se alguém já baniu o campeão
-          if (!banChampion.force && teamPicks.some(ally => ally.championPickIntent == championId)) { continue }  // se o force tá desativado, se algum aliado quer o campeão
+          if (!banChampion.force && myTeam.some(ally => ally.championPickIntent == championId)) { continue }  // se o force tá desativado, se algum aliado quer o campeão
           if (await requests.selectChampion(action.id, championId)) { return }
         }
       }
