a70b4b8ce5c104434e93c12297035087e413d7c9
balaclava <yangabrielogy@gmail.com>
2023-07-04 19:03:16 -0300

    refactor: menos linhas de código

--- front.js
+++ front.js
@@ -6,24 +6,23 @@
  */
 
 export function getDropdown(dropdownId) {
-    const dropdown = document.createElement("lol-uikit-framed-dropdown")
-    dropdown.id = dropdownId
-    dropdown.style.marginBottom = "1.2px"
-    return dropdown
+  const dropdown = document.createElement("lol-uikit-framed-dropdown");
+  dropdown.setAttribute("style", "margin-bottom: 1.2px;");
+  dropdown.id = dropdownId;
+  return dropdown;
 }
 
 export function getOption(text) {
-    const option = document.createElement("lol-uikit-dropdown-option")
-    option.setAttribute("slot", "lol-uikit-dropdown-option")
-    option.textContent = text
-    return option
+  const option = document.createElement("lol-uikit-dropdown-option");
+  option.setAttribute("slot", "lol-uikit-dropdown-option");
+  option.textContent = text;
+  return option;
 }
 
 export function getCheckBox(text, enabled) {
-    const pickCheckbox = document.createElement("lol-uikit-radio-input-option")
-    if (enabled) { pickCheckbox.setAttribute("selected", enabled) }
-    pickCheckbox.style.marginLeft = "16px"
-    pickCheckbox.style.fontFamily = "Arial"
-    pickCheckbox.textContent = text
-    return pickCheckbox
+  const pickCheckbox = document.createElement("lol-uikit-radio-input-option");
+  if (enabled) { pickCheckbox.setAttribute("selected", "true"); }
+  pickCheckbox.setAttribute("style", "margin-left: 16px; font-family: Arial;");
+  pickCheckbox.textContent = text;
+  return pickCheckbox;
 }
