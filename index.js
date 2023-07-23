dbafe9c7163b322b9f01482ae690a1718a31b442
Yan Gabriel <yangabrielogy@gmail.com>
2023-07-23 02:39:35 -0300

    fix: index adicionado

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author balaclava
+ * @name auto-champion-select
+ * @link https://github.com/controlado/auto-champion-select
+ * @description Pick or ban automatically! 🐧
+ */
+
+import "https://cdn.skypack.dev/auto-champion-select@latest?min";
